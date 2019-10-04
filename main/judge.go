821840989449c5abac4dd6b591743c23efa48ae9
EspiDev <espidev@gmail.com>
2019-10-03 22:34:30 -0400

    Added case judger

--- main/judge.go
+++ main/judge.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"bufio"
 	pb "github.com/BayviewComputerClub/smoothie-runner/protocol"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
-	"runtime"
+	"strings"
 )
+
+const (
+	OUTCOME_AC  = "AC"  // answer correct
+	OUTCOME_WA  = "WA"  // wrong answer
+	OUTCOME_RTE = "RTE" // run time error
+	OUTCOME_CE  = "CE"  // compile error
+	OUTCOME_TLE = "TLE" // time limit exceeded
+	OUTCOME_MLE = "MLE" // memory limit exceeded
+)
+
+type CaseReturn struct {
+	Result     string
+	ResultInfo string
+}
 
 /*
  * Tasks:
@@ -18,17 +35,169 @@ import (
  * set timeout
  */
 
-func judgeCase(compiledProgram os.File, batchCase pb.ProblemBatchCase) pb.TestCaseResult {
-	c := exec.Command("./" + compiledProgram.Name())
+func judgeStdoutListener(reader *io.ReadCloser, done chan CaseReturn, expectedAnswer *string) {
+	buff := bufio.NewReader(*reader)
 
+	expectedScanner := bufio.NewScanner(strings.NewReader(*expectedAnswer))
+	expectedScanner.Scan()
+
+	expectedLine := []rune(strings.ReplaceAll(expectedScanner.Text(), "\r", ""))
+	expectedLineIndex := 0
+	expectingEnd := false
+	ignoreSpacesIndex := 0
+
+	// calculate when to start ignoring spaces (at the end of the line before \n or \r\n
+	for i := len(expectedLine) - 1; i >= 0; i-- {
+		if expectedLine[i] != '\n' && expectedLine[i] != ' ' {
+			ignoreSpacesIndex = i + 1
+			break
+		}
+	}
+
+	// loop through to read rune by rune
+	for {
+
+		// read rune to parse
+		c, _, err := buff.ReadRune()
+
+		// check for errors to return
+		if err != nil {
+			if err == io.EOF { // no more output
+				if expectingEnd { // expected no more text
+					done <- CaseReturn{
+						Result: OUTCOME_AC,
+					}
+				} else { // did not finish giving full answer
+					done <- CaseReturn{
+						Result: OUTCOME_WA,
+					}
+				}
+				return
+			} else { // other errors
+				done <- CaseReturn{
+					Result:     OUTCOME_RTE,
+					ResultInfo: err.Error(), // TODO
+				}
+				return
+			}
+		}
+
+		// validate obtained rune
+
+		if expectingEnd { // if expecting no more text
+
+			if c != '\n' && c != ' ' { // if not new line and space
+				done <- CaseReturn{
+					Result:     OUTCOME_WA,
+					ResultInfo: "bruh",
+				}
+				return
+			}
+
+		} else if c == '\n' { // if new line is detected
+
+			if expectedLineIndex >= ignoreSpacesIndex { // if waiting for new line
+
+				if expectedScanner.Scan() { // if there is more expected output
+					// reset for next line of reading
+					expectedLine = []rune(strings.ReplaceAll(expectedScanner.Text(), "\r", ""))
+					ignoreSpacesIndex = 0
+					expectedLineIndex = -1 // it adds at end
+
+					// calculate when to start ignoring spaces (at the end of the line before \n or \r\n
+					for i := len(expectedLine) - 1; i >= 0; i-- {
+						if expectedLine[i] != '\n' && expectedLine[i] != ' ' {
+							ignoreSpacesIndex = i + 1
+							break
+						}
+					}
+				} else { // if there isn't
+					expectingEnd = true
+				}
+
+			} else { // if not waiting for new line
+				done <- CaseReturn{
+					Result: OUTCOME_WA,
+				}
+				return
+			}
+
+		} else if expectedLineIndex >= len(expectedLine) || c != expectedLine[expectedLineIndex] { // if character did not match expected character
+
+			if !(expectedLineIndex >= ignoreSpacesIndex && c == ' ') { // if not waiting for space or new line, or character is not space or new line
+				done <- CaseReturn{
+					Result: OUTCOME_WA,
+				}
+				return
+			}
+		}
+
+		expectedLineIndex++
+	}
+}
+
+func judgeStderrListener(reader *io.ReadCloser, done chan CaseReturn) {
+	str, err := ioutil.ReadAll(*reader)
+
+	if err != nil { // should terminate peacefully
+		warn("Stderr: " + err.Error()) // TODO
+	} else {
+		done <- CaseReturn{
+			Result: OUTCOME_RTE,
+			ResultInfo: string(str),
+		}
+	}
+}
+
+func judgeStdinFeeder(writer *io.WriteCloser, done chan CaseReturn, feed *string) {
+	buff := bufio.NewWriter(*writer)
+	_, err := buff.WriteString(*feed)
+	if err != nil {
+		done <- CaseReturn{
+			Result: OUTCOME_RTE,
+		}
+		warn("Stdin: " + err.Error()) // TODO
+		return
+	}
+}
+
+func judgeCase(compiledProgram os.File, batchCase *pb.ProblemBatchCase) *pb.TestCaseResult {
+	c := exec.Command("./" + compiledProgram.Name())
+	done := make(chan CaseReturn)
+
+	// initialize pipes
 	stderrPipe, err := c.StderrPipe()
 	if err != nil {
 		warn(err.Error())
-		return
+		return nil
 	}
 	stdoutPipe, err := c.StdoutPipe()
+	if err != nil {
+		warn(err.Error())
+		return nil
+	}
 	stdinPipe, err := c.StdinPipe()
+	if err != nil {
+		warn(err.Error())
+		return nil
+	}
 
+	// start listener pipes
+	go judgeStdoutListener(&stdoutPipe, done, &batchCase.ExpectedAnswer)
+	go judgeStderrListener(&stderrPipe, done)
+
+	// start process
 	err = c.Start()
+	if err != nil {
+		warn(err.Error())
+		return nil
+	}
+
+	// feed input to process
+	go judgeStdinFeeder(&stdinPipe, done, &batchCase.Input)
+
+	// wait for judging to finish
+	response := <-done
+
 
 }
