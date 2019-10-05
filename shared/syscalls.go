9453d2b28058e50a5a090d7ec22119ce2eb1bc3f
EspiDev <espidev@gmail.com>
2019-10-05 15:43:58 -0400

    added syscalls

--- shared/syscalls.go
+++ shared/syscalls.go
@@ -5,10 +5,13 @@ import "golang.org/x/sys/unix"
 var ALLOWED_CALLS = []uint64{
 	unix.SYS_READ,
 	unix.SYS_WRITE,
+	unix.SYS_WRITEV,
 	unix.SYS_EXIT,
 	unix.SYS_STAT,
 	unix.SYS_FSTAT,
+	unix.SYS_STATFS,
 	unix.SYS_CLOSE,
+
 	unix.SYS_RT_SIGRETURN,
 	unix.SYS_OPENAT,
 	unix.SYS_WRITEV,
@@ -16,9 +19,19 @@ var ALLOWED_CALLS = []uint64{
 	unix.SYS_BRK,
 	unix.SYS_NANOSLEEP,
 	unix.SYS_CLOCK_NANOSLEEP,
+
+	unix.SYS_DUP,
+	unix.SYS_DUP2,
+	unix.SYS_DUP3,
+
+	unix.SYS_FSTAT,
+
 	unix.SYS_MMAP,
+	unix.SYS_MREMAP,
 	unix.SYS_MPROTECT,
+	unix.SYS_MADVISE,
+	unix.SYS_MUNMAP,
+
 	unix.SYS_ACCESS,
 	unix.SYS_ARCH_PRCTL,
-	unix.SYS_MUNMAP,
 }
